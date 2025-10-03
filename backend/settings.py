5896eeb505d2600622e7b1e7a77b23e38d2d8d6f
Nathan Blanch <nldblanch@gmail.com>
2025-10-03 17:03:16 +0100

    update settings again

--- backend/settings.py
+++ backend/settings.py
@@ -9,9 +9,14 @@ class Settings(BaseSettings):
     # Required
     DATABASE_URL: str
     PROJECT_NAME: str
-    
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
     FRONTEND_HOST: str = "http://localhost:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+
     SECRET_KEY: str
     ALGORITHM: str
     
