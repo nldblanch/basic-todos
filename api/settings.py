b4e1ea0b03655bbe3ff1e45a33b957042dbce36c
Nathan Blanch <nldblanch@gmail.com>
2025-10-03 13:28:00 +0100

    attempt 2

--- api/settings.py
+++ api/settings.py
@@ -16,10 +16,7 @@ class Settings(BaseSettings):
     # Required
     DATABASE_URL: str
     PROJECT_NAME: str
-    POSTGRES_SERVER: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str = ""
-    POSTGRES_DB: str = ""
+    
     FRONTEND_HOST: str = "http://localhost:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
     SECRET_KEY: str
